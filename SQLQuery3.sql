d47fe94bf8ec9347d743feee30a6f3966fa7662c
dianacopacondori <cvdianacarolina26@gmail.com>
2025-08-20 22:56:51 -0500

    Todas las tablas completas

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -23,6 +23,28 @@ cargo varchar(15),
 usuario varchar(15),
 contraseña numeric (5));
 
+create table Cliente (
+idCliente int identity (1,1) not null PRIMARY KEY,
+nombre varchar (30),
+dni numeric (8),
+telefono numeric (10),
+dirección varchar (30));
+
 create table Venta (
-idVenta int not null PRIMARY KEY,
-fecha date);
+idVenta int identity (1,1) not null PRIMARY KEY,
+fecha date,
+idCliente int,
+idEmpleado int,
+CONSTRAINT FK_Venta_idCliente FOREIGN KEY (idCliente) REFERENCES Cliente(idCliente),
+CONSTRAINT FK_Venta_idEmpleado FOREIGN KEY (idEmpleado) REFERENCES Empleado(idEmpleado));
+
+create table DetalleVenta (
+idDetalle int identity (1,1) not null PRIMARY KEY,
+idVenta int,
+idProducto int,
+cantidad int,
+precioUnitario money,
+subtotal money,
+CONSTRAINT FK_DetalleVenta_idVenta FOREIGN KEY (idVenta) REFERENCES Venta(idVenta),
+CONSTRAINT FK_DetalleVenta_idProducto FOREIGN KEY (idProducto) REFERENCES Productos(idProducto));
+
