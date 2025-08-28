e70af178b4451357de4c22069af4a950ca4a9c19
dianacopacondori <cvdianacarolina26@gmail.com>
2025-08-27 23:51:35 -0500

    insert values Empleado, Clientes

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -29,6 +29,9 @@ dni numeric (8),
 telefono numeric (10),
 dirección varchar (30));
 
+Alter table Cliente alter column dni char (8);
+alter table Cliente alter column telefono char (10);
+
 create table Venta (
 idVenta int identity (1,1) not null PRIMARY KEY,
 fecha date,
@@ -47,13 +50,31 @@ subtotal money,
 CONSTRAINT FK_DetalleVenta_idVenta FOREIGN KEY (idVenta) REFERENCES Venta(idVenta),
 CONSTRAINT FK_DetalleVenta_idProducto FOREIGN KEY (idProducto) REFERENCES Productos(idProducto));
 
-insert into Cliente values (
-'Samuel', 29638765, 935253886, 'Av. manz1');
-select * from Cliente;
+insert into Cliente values 
+('Samuel Ramirez', 29638765, 935253886, 'Av. Los Olivos 123'),
+('Ester Paredes',28656438, 947599035, 'Jr. Las Flores 456'),
+('Carlos Mendoza', 36987656, 963422178, 'Calle Lima'),
+('Ana Torres', 37625399, 932176389, 'Av.Arequipa 351'),
+('Jorge Salazar', 47327865, 936599678, 'Jr. Cusco 624'),
+('Maria Quispe', 28673657, 905134055, 'Calle Sol 386'),
+('Pedro Gutierrez', 27597689, 915607566, 'Av. San Martin 145'),
+('Sofia Chavez', 45237865, 970053189,  'Jr. Libertad 852');
 
-insert into Empleado values (
-'Bruno', 'cajero', 'cjBruno', 36788);
-insert into Empleado values (
-'Camila', 'repositor', 'rpCamila', 40788);
-select * from Empleado;
+select * from Cliente
+ORDER BY nombre ASC;
+
+
+select * from Cliente where nombre = 'Carlos Mendoza';
+
+insert into Empleado values
+('Camila', 'gerente', 'grCamila', 05382),
+('Diego', 'repositor', 'rpDiego', 56382),
+('Lucia', 'analista', 'anLucia', 62980),
+('Mateo', 'vendedor', 'vdMateo', 74900),
+('Sofia', 'supervisor', 'spSofia', 01563),
+('Valeria','aistente', 'asValeria', 67493),
+('Javier', 'logistica', 'lgJavier', 64830),
+('Bruno', 'cajero', 'cjBruno', 36788);
+
+select cargo, usuario from Empleado;
 
